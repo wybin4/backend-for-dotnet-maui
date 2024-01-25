3e912fae2100d0f806a5c707ad85abcb56c7d0ac
wybin4 <eckyl@bk.ru>
2024-01-25 21:08:21 +0300

    25.01.23 порт

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 
 const app = express();
-const port = 80;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(cors());
