b5b065fa83b107aca59b9276430e2a61506e3fc0
wybin4 <eckyl@bk.ru>
2024-01-25 20:58:25 +0300

    25.01.23 пример

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-// app.js
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
 
-const express = require('express')
-const app = express()
+const app = express();
+const port = 3000;
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-module.exports = app
