01d65fd7291f23a1e861949c12c81467850a7033
wybin4 <eckyl@bk.ru>
2024-01-25 20:44:04 +0300

    25.01.23 начало работы

---
+++ index.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+mongoose.connect('mongodb://admin:admin@localhost:27017', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const Item = mongoose.model('Item', {
+    _id: mongoose.Types.ObjectId,
+    text: String,
+    description: String,
+    date: { type: String, default: new Date().toLocaleDateString() },
+    importance: { type: Number, default: 50 },
+    category: String
+});
+
+// const Category = mongoose.model('Category', {
+//     _id: mongoose.Types.ObjectId,
+//     title: String,
+//     description: String,
+//     success_rate: { type: Number, default: 50 }
+// });
+
+app.get('/api/items', async (req, res) => {
+    const items = await Item.find();
+    res.json(items);
+});
+
+app.get('/api/items/:id', async (req, res) => {
+    const item = await Item.findOne({ _id: new mongoose.Types.ObjectId(req.params.id) });
+    if (!item) {
+        return res.status(404).json({ error: 'Item not found' });
+    }
+    res.json(item);
+});
+
+app.post('/api/items', async (req, res) => {
+    const newItem = new Item({ _id: new mongoose.Types.ObjectId(), ...req.body });
+    await newItem.save();
+    res.json(newItem);
+});
+
+app.put('/api/items/:id', async (req, res) => {
+    const existingItem = await Item.findOne({ _id: new mongoose.Types.ObjectId(req.params.id) });
+    if (!existingItem) {
+        return res.status(404).json({ error: 'Item not found' });
+    }
+
+    existingItem.set(req.body);
+    await existingItem.save();
+    res.json(existingItem);
+});
+
+app.delete('/api/items/:id', async (req, res) => {
+    const item = await Item.findByIdAndDelete({ _id: req.params.id });
+    if (!item) {
+        return res.status(404).json({ error: 'Item not found' });
+    }
+    res.json(item);
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
